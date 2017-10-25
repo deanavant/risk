3e24bafb4ac0b7727a5e8254fab45d2cddc2584d
dt_avant@yahoo.com <dt_avant@yahoo.com>
2017-10-25 15:14:15 -0700

    added context to home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using risk.Models;
 
 namespace risk.Controllers
 {
     public class HomeController : Controller
     {
+        private RiskContext _context;
+
+        public HomeController (RiskContext context)
+        {
+            _context = context;
+        }
+
         // GET: /Home/
         [HttpGet]
         [Route("")]
