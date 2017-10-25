2a583cec419ad3a9110875bb9358048972c4517c
JohnLeibowitz <jhleibow@gmail.com>
2017-10-25 13:53:06 -0700

    revised models to match database

--- Models/RiskContext.cs
+++ Models/RiskContext.cs
@@ -2,10 +2,10 @@ using Microsoft.EntityFrameworkCore;
  
 namespace risk.Models
 {
-    public class BrightIdeasContext : DbContext
+    public class RiskContext : DbContext
     {
         // base() calls the parent class' constructor passing the "options" parameter along
-        public BrightIdeasContext(DbContextOptions<BrightIdeasContext> options) : base(options) { }
+        public RiskContext(DbContextOptions<RiskContext> options) : base(options) { }
         public DbSet<Player> player { get; set;}
         public DbSet<Territory> territory { get; set;}
         public DbSet<Game> game { get; set;}
--- Models/Territory.cs
+++ Models/Territory.cs
@@ -1,9 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 using System;
 
 namespace risk.Models
 {
     public class Territory
     {
-        public int Id { get; set; }
+        [Key]
+        public int id { get; set; }
+        
+        [Required]
+        [MinLength(1)]
+        public string name { get; set; }
+        
+        [ForeignKey("owner_id")]
+        public Player owner { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime created_at { get; set; }
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime updated_at { get; set; }
     }
 }
