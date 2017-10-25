7ebcb2227f71ae3fd7ebc5a5402f98a691365eb6
Dean Avant <dt_avant@yahoo.com>
2017-10-25 11:33:38 -0700

    updated game with player list

--- Models/Game.cs
+++ Models/Game.cs
@@ -6,7 +6,9 @@ namespace risk.Models
     public class Game
     {
         public int Id { get; set; }
+        public string State { get; set; }
         public List<Territory> territories { get; set; }
+        public List<Player> players { get; set; }
 
         public Game()
         {
