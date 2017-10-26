72e5fe23f4d5ce946fee166929fe9ebabc2b7108
ritud <ritu.dutta26@gmail.com>
2017-10-26 14:21:12 -0700

    Modified View and Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace risk.Controllers
         public IActionResult Index()
         {
             Game myGame = new Game();
+            ViewBag.game = myGame.territories;
             Console.Write(myGame.territories["Peru"].name);
             
             Console.WriteLine(myGame.territories["Peru"].bottomRightX);
