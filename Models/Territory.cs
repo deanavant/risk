576568dbe392f4449eb663176fbc8fbeca86a0c1
dt_avant@yahoo.com <dt_avant@yahoo.com>
2017-10-26 11:08:10 -0700

    replaced methods in territory.cs

--- Models/Territory.cs
+++ Models/Territory.cs
@@ -24,10 +24,47 @@ namespace risk.Models
         public DateTime updated_at { get; set; }
 
         [NotMapped]
-
         public int topLeftX { get; set; }
 
+        [NotMapped]
+        public List<Territory> neighbors { get; set; }
 
+        [NotMapped]
+        public int armies { get; set; }
 
+        public Territory()
+        {
+            neighbors = new List<Territory>();
+            armies = 0;
+        }
+
+        public bool AddNeighbor(Territory n)
+        {
+            if(!neighbors.Contains(n) )
+            {
+                neighbors.Add(n);
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsNeighbor(Territory n)
+        {
+            return neighbors.Contains(n);
+        }
+        public bool IsFoe(Territory n)
+        {
+            return owner != n.owner;
+        }
+
+        public bool AddArmies(Player player, int armies = 1)
+        {
+            if(armies != 0)
+            {
+                this.armies = armies;
+                return true;
+            }
+            return false;
+        }
     }
 }
